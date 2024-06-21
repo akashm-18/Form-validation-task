7eec35e350fc84a62904bb71ff57d67c11a1761e
akashm-18 <akashmurugan338@gmail.com>
2024-06-21 11:51:34 +0530

    "Updated the displayData component"

--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -7,7 +7,7 @@ const DisplayData = () => {
   return (
     <div className='w-full min-h-screen flex items-center justify-center bg-gray-100'>
     <div className='p-6 bg-white rounded shadow-md'>
-      <h1 className='text-2xl mb-4'>Submitted Data</h1>
+      <h1 className='text-2xl mb-4'>Submitted data for your Reference</h1>
       <p><strong>Name:</strong> {name}</p>
       <p><strong>Email:</strong> {email}</p>
       <p><strong>Password:</strong> {password}</p>
